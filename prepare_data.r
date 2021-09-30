1e77c5a2f07e394691bfc9f49bf2fbbe5adf675c
Robert M Branton <bob.branton@gmail.com>
2021-09-30 18:19:14 -0300

    Update prepare_data.r
    update species metadata

--- prepare_data.r
+++ prepare_data.r
@@ -18,16 +18,18 @@ library(sf)
 
 extract<-FALSE
 pNames<-c('ala','bison','gbif','eBird','idigbio','obis','VertNet')
+
 qNames<-c('Carcinus maenas','Caulerpa taxifolia','Codium fragile','Dreissena polymorpha','Mnemiopsis leidyi','Pterois volitans','Rapana venosa')
 cNames<-c('European Green Crab','Killer Algae','Dead Mans Fingers','Zebra Mussel','Sea Walnut','Lion Fish','Veined Rapa Whelk') # invasive
+wikidata<-c('Q27779','Q310961','Q2712208','Q752130','Q133630','Q824672','Q139053')
 sColors<-c('green','cyan','magenta','red','orange','yellow','dodgerblue')
 fImages<-c('https://upload.wikimedia.org/wikipedia/commons/1/17/Carcinus_maenas.jpg',
-           'https://inaturalist-open-data.s3.amazonaws.com/photos/9434207/medium.jpg?1501502101',
+           'https://upload.wikimedia.org/wikipedia/commons/e/e7/CaulerpaTaxifolia.jpg',
            'https://upload.wikimedia.org/wikipedia/commons/e/ed/Codiumfragile.jpg',
-           'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Dreissena_polymorpha_(Zebra_mussel),_Arnhem,_the_Netherlands.jpg/500px-Dreissena_polymorpha_(Zebra_mussel),_Arnhem,_the_Netherlands.jpg',
-           'https://inaturalist-open-data.s3.amazonaws.com/photos/9566101/medium.jpg?1545697239',
-           'https://static.inaturalist.org/photos/13375992/medium.jpg?1518045182',
-           'https://inaturalist-open-data.s3.amazonaws.com/photos/5026554/medium.jpg?1474961472')
+           'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Dreissena_polymorpha3.jpg/220px-Dreissena_polymorpha3.jpg',
+           'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Sea_walnut%2C_Boston_Aquarium.jpg/220px-Sea_walnut%2C_Boston_Aquarium.jpg',
+           'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Pterois_volitans_Manado-e_edit.jpg/220px-Pterois_volitans_Manado-e_edit.jpg',
+           'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Rapana_Black_Sea_2008_G1.jpg/220px-Rapana_Black_Sea_2008_G1.jpg')
 
 species.style<-data.frame(Names=qNames,cNames=cNames,sColors=sColors,fImages=fImages,stringsAsFactors = FALSE)
 
